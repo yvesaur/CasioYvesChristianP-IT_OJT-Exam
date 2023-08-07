c58797e612f515de4d0f6529cf22979aded2f396
yvesaur <yveschristianpcasio@gmail.com>
2023-08-08 01:49:58 +0800

    style: Add Email List Boilerplate, Logo change, Dummy Data

--- email-viewer/src/App.tsx
+++ email-viewer/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Emails from "./components/Emails";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Emails></Emails>
     </div>
   );
 };
--- email-viewer/src/components/Emails.tsx
+++ email-viewer/src/components/Emails.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./Emails.css";
 
 const Emails = () => {
-  return <div>Emails</div>;
+  return (
+    <div id="email-container">
+      <div className="unread-emails">
+        <p>Unread</p>
+        <span className="notification-badge">3</span>
+      </div>
+    </div>
+  );
 };
 
 export default Emails;
